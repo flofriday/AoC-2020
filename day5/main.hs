10c190ad9e1fd4f71dc960742b53723b80f9c27d
flofriday <flohacksfriday@gmail.com>
2020-12-05 14:25:19 +0100

    Add comments for day 5

--- day5/main.hs
+++ day5/main.hs
@@ -1,3 +1,4 @@
+-- Convert the column description to a y cordinate
 toX :: String -> [Int] -> [Int]
 toX [] range = range
 toX (c : cs) range
@@ -6,6 +7,7 @@ toX (c : cs) range
   where
     half = length range `div` 2
 
+-- Convert the row description to a y cordinate
 toY :: String -> [Int] -> [Int]
 toY [] range = range
 toY (c : cs) range
@@ -14,26 +16,32 @@ toY (c : cs) range
   where
     half = length range `div` 2
 
+-- Convert the description to a x and y cordinate tupel
 toCordinate :: String -> (Int, Int)
 toCordinate text = (x, y)
   where
     y = head (toY (take 7 text) [0 .. 127])
     x = head (toX (drop 7 text) [0 .. 7])
 
+-- Convert a cordinate to a Id
 toId :: (Int, Int) -> Int
 toId val = x + y * 8
   where
     (x, y) = val
 
+-- Create a list with all seats of the plane as (x,y) tupels
 allSeats :: [Int] -> [Int] -> [(Int, Int)]
 allSeats [] _ = []
 allSeats (x : xs) ys = zip (replicate (length ys) x) ys ++ allSeats xs ys
 
+-- Find all free seats (or maybe not existing seats) and return them as a list
+-- of (x,y) tupels
 freeSeats :: [(Int, Int)] -> [(Int, Int)]
 freeSeats taken = filter (`notElem` taken) all
   where
     all = allSeats [0 .. 7] [0 .. 127]
 
+-- Filter all non existing seatIds, so that only free existing ones remain
 filterFreeIds :: [Int] -> [Int] -> [Int]
 filterFreeIds free taken = filter (\x -> x + 1 `elem` taken && x - 1 `elem` taken) free
 
